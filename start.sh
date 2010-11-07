740b7eb61660864238a660ced65e6d66c9035478
barce <jbarcelona@gmail.com>
2010-11-06 17:24:18 -0700

    making paths easy to change for customization

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-export NODE_PATH="$NODE_PATH:/home/markgunnels75/noylitics/deps/node-expat/build/default:/home/markgunnels75/noylitics/deps/node-xmpp/lib/:/home/markgunnels75/noylitics/deps/node-base64/build/default"
+export NODE_EXPAT="/home/markgunnels75/noylitics/deps/node-expat/build/default"
+export NODE_XMPP="/home/markgunnels75/noylitics/deps/node-xmpp/lib/"
+export NODE_BASE64="/home/markgunnels75/noylitics/deps/node-base64/build/default"
+export NODE_PATH="$NODE_PATH:$NODE_EXPAT:$NODE_XMPP:$NODE_BASE64"
