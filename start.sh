5350339631a397a9d99af85a448fe1a87eeb7a8d
barce <jbarcelona@gmail.com>
2010-11-06 23:55:10 -0700

    abstracting out deps path

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 export DEPS="/home/markgunnels75/noylitics/deps"
 export NODE_EXPAT="$DEPS/node-expat/build/default"
+export NODE_XMLJS="$DEPS/libxmljs"
+export NODE_XML="$DEPS/node-xml/lib"
+export NODE_TWITTER="$DEPS/twitter-node/lib/twitter-node"
+export NODE_HTMLPARSER="$DEPS/node-htmlparser"
 export NODE_XMPP="$DEPS/node-xmpp/lib/"
 export NODE_BASE64="$DEPS/node-base64/build/default"
-export NODE_PATH="$NODE_PATH:$NODE_EXPAT:$NODE_XMPP:$NODE_BASE64"
+export NODE_PATH="$NODE_PATH:$NODE_EXPAT:$NODE_XMLJS:$NODE_XML:$NODE_XMPP:$NODE_BASE64:$NODE_EXPAT/node-expat:$NODE_HTMLPARSER:$NODE_TWITTER"
+
